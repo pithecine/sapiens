4030431e7a81196c831de4d5653b0903c697c75b
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-01-26 15:08:11 -0500

    use reflection to inject params

--- src/main/kotlin/pithecine/sapiens/core/FeatureCalc.kt
+++ src/main/kotlin/pithecine/sapiens/core/FeatureCalc.kt
@@ -2,7 +2,7 @@ package pithecine.sapiens.core
 
 class FeatureCalc {
 
-  operator fun invoke(a: Double, b: Double): Double{
-    return a*b
+  operator fun invoke(a: Double, b: Double): Double {
+    return a * b
   }
 }
--- src/test/kotlin/pithecine/sapiens/core/FeatureCalcTest.kt
+++ src/test/kotlin/pithecine/sapiens/core/FeatureCalcTest.kt
@@ -5,19 +5,24 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class FeatureCalcTest {
-  @Test fun testInjectInvoke() {
+  @Test
+  fun testInjectInvoke() {
     val features = mapOf("a" to 5.5, "b" to 6.7)
-    val calc = FeatureCalc()
     val callable: KCallable<Double> = FeatureCalc::invoke
     val params = callable.parameters
 
     val a = params[1].name
     val b = params[2].name
     assertEquals(3, params.size)
-    assertEquals("a",a)
-    assertEquals("b",b)
+    assertEquals("a", a)
+    assertEquals("b", b)
 
-    val product = calc(features[a]!!.toDouble() , features[b]!!.toDouble())
-    assertEquals(5.5*6.7, product)
+    val namedParams = listOf(params[1].name, params[2].name)
+    val opaqueParams: List<Any> = namedParams.map {
+      features[it]!!.toDouble()
+    }
+
+    val product = callable.call(FeatureCalc(), *opaqueParams.toTypedArray())
+    assertEquals(5.5 * 6.7, product)
   }
 }
